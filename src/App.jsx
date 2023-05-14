3f707626067fc7c9920c00934360f9e09e1591e1
ParthibBr <bparthib77@gmail.com>
2023-05-14 09:17:38 +0600

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import './App.css'
-import ExerciseItems from './components/ExerciseItems/ExerciseItems'
-import SideBar from './components/SideBar/SideBar'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import ExerciseItems from "./components/ExerciseItems/ExerciseItems";
+import SideBar from "./components/SideBar/SideBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <div className='mainContainer'>
-        <ExerciseItems></ExerciseItems>
-        <SideBar></SideBar>
+      <div className="mainContainer">
+        <div className="row w-100">
+          <div className="col-9">
+            <ExerciseItems></ExerciseItems>
+          </div>
+          <div className="col-3">
+            <SideBar></SideBar>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import img from "../../images/rdj.jpg";
 import "./SideBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = () => {
   return (
     <div className="sideBar bg-success">
       <div className="aboutme">
         <div className="row">
-          <div className="col-3">
+          <div className="col-2">
             <img src={img} alt="" />
           </div>
-          <div className="col-9">
-            <p>Zahid Hossain</p>
+          <div className="col-10 ps-4">
+            <h6>Zahid Hossain</h6>
             <span>
-              
+              <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
+              Chittagong,Bangladesh
             </span>
-            <span>Chittagong,Bangladesh</span>
+          </div>
+        </div>
+      </div>
+      <div className="bodyInfo mt-5">
+        <div className="card">
+          <div className="card-body">
+            <div className="row text-center ju">
+              <div className="col-4">
+                <h5>75Kg</h5>
+                <p>Weight</p>
+              </div>
+              <div className="col-4">
+                <h5>6.5</h5>
+                <p>Height</p>
+              </div>
+              <div className="col-4">
+                <h5>25Yr</h5>
+                <p>Weight</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
